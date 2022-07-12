5743d4753741646d9feecab12fa52e144ce59c7e
anujsinghrawat <2003013019@ipec.org.in>
2022-07-12 10:36:41 +0530

    day 3 completed
    Signed-off-by: anujsinghrawat <2003013019@ipec.org.in>

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,17 +4,55 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assets/images/login_image.png",
-          fit: BoxFit.cover,),
-          Text("Welcome",style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),)
-        ],
-      )
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Naam daalo bhai apna",
+                      labelText: "ShubhNaam",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Password daalo bhai apna",
+                      labelText: "Secret Key",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+
+                  ElevatedButton(onPressed: (){}, child: Text("Andar chalo humare saath  "),
+                  style: TextButton.styleFrom(),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
