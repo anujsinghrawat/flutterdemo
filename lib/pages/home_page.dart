ed40ddfbe21d7bc3bc73c8b20da4f92f4401ebb0
anujsinghrawat <2003013019@ipec.org.in>
2022-07-14 12:52:53 +0530

    Day11
    Signed-off-by: anujsinghrawat <2003013019@ipec.org.in>

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutterdemo/models/catalog.dart';
 
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
-
+  final int days = 69;
+  final String name = "call me daddy";
   @override
   Widget build(BuildContext context) {
-    final int days = 69;
-    final String name = "call me daddy";
+    final dummyList = List.generate(
+      50,
+      (index) => CatelogModel.items[0],
+    );
 
     return Scaffold(
       appBar: AppBar(
-        
-        title: Text("khareedi",
-        
+        title: Text(
+          "khareedi",
         ),
       ),
-      body: Center(
-          child: Container(
-        child: Text("kya haal hai female grils aao $days kare or $name "),
-      )),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            }),
+      ),
       drawer: MyDrawer(),
     );
   }
